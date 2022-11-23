a0c80fa809cdb48d3115b2b26711485969650e07
Florin <florin@academiatestarii.ro>
2022-11-23 10:46:46 +0200

    Formular + Validari

--- app.py
+++ app.py
@@ -1,40 +1,66 @@
-import datetime
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, url_for
+from forms import CourseSimulator
+import simulations
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = 'your secret key'
+
+CourseName = []
+CourseHours = []
+CourseCost = []
+CourseCompetitionRate = []
+CourseLicenseCost = []
+CourseStudents = []
+CourseIterations = []
+CourseTrainer = []
+CourseTrainerRate = []
+CourseStudentOperationalCost = []
+courseMarketingCost = []
+courseDeliveryCost = []
+courseTotalLicenseCost = []
+courseTotalOperationalCost = []
+courseTotalRevenue = []
+courseTotalCosts = []
+courseProfit = []
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = CourseSimulator()
+    if form.validate_on_submit():
+        CourseName.append(form.denumireCurs.data)
+        CourseHours.append(form.numarOre.data)
+        CourseCost.append(form.costCurs.data)
+        CourseCompetitionRate.append(form.costCompetitie.data)
+        CourseLicenseCost.append(form.costLiceenta.data)
+        CourseStudents.append(form.students.data)
+        CourseIterations.append(form.iteratii.data)
+        CourseTrainer.append(form.numeProfesor.data)
+        CourseTrainerRate.append(form.tarifOrar.data)
+        CourseStudentOperationalCost.append(form.costOperational.data)
+        courseMarketingCost.append(form.costMarketing.data)
+        for i in CourseName:
+            x = int(CourseTrainerRate[i]) * int(CourseHours[i])
+            courseDeliveryCost.append(x)
+            t = int(CourseLicenseCost[i]) * int(CourseStudents[i]) * int(CourseIterations[i])
+            courseTotalLicenseCost.append(t)
+            p = int(CourseStudentOperationalCost[i]) * int(CourseStudents[i]) * int(CourseIterations[i])
+            courseTotalOperationalCost.append(p)
+            y = int(CourseCost[i]) * int(CourseStudents[i]) * int(CourseIterations[i])
+            courseTotalRevenue.append(y)
+            m = int(courseMarketingCost[i]) + int(courseTotalLicenseCost[i]) + int(courseDeliveryCost[i]) + int(courseTotalOperationalCost[i])
+            courseTotalCosts.append(m)
+            v = courseTotalRevenue - courseTotalCosts
+            courseProfit.append(v)
+            return courseDeliveryCost, courseTotalLicenseCost, courseTotalOperationalCost, courseTotalRevenue, courseTotalCosts, courseProfit
+        return redirect(url_for('coursesKPI'))
+    return render_template('index.html', form=form)
 
 
-@app.route('/courseSimulation/')
-def courseSimulation():
-    return render_template('courseSimulation.html')
-
-
-@app.route('/courseSimulation/', methods=['POST'])
-def courseSimulation_post():
-    pret = request.form['pret']
-    durata = request.form['durata']
-    costLiceenta = request.form['license']
-    rate = request.form['rate']
-    studenti = request.form['studenti']
-    iteratii = request.form['iteratii']
-    marketing = request.form['marketing']
-    operational = request.form['operational']
-    competitionHourly = request.form['competition']
-    courseHourCost = (int(pret) / int(durata)) / 5
-    hourlyComparison = (int(courseHourCost) / int(competitionHourly))
-    revenue = (int(studenti) * int(iteratii) * int(pret)) / 5
-    calculateLicenseCost = int(costLiceenta) * int(studenti) * int(iteratii)
-    calculateDeliveryCost = (int(rate) * int(durata) * int(iteratii))
-    calculateOperationalCost = (int(operational) * int(studenti) * int(iteratii))
-    costs = calculateDeliveryCost + int(marketing) + calculateOperationalCost + calculateLicenseCost
-    profit = revenue - costs
-    profitMargin = (int(profit) / int(revenue))
-    return render_template('result.html', revenue=revenue, costs=costs, profit=profit, margin=profitMargin,
-                           hourly=courseHourCost, competition=hourlyComparison, licenseCost=calculateLicenseCost,
-                           deliveryCost=calculateDeliveryCost, operationalCost=calculateOperationalCost)
+@app.route('/coursesKPI/')
+def coursesKPI():
+    return render_template('coursesKPI.html', CourseName=CourseName, CourseCost=CourseCost, CourseHours=CourseHours, CourseCompetitionRate=CourseCompetitionRate,
+                           CourseLicenseCost=CourseLicenseCost, CourseStudents=CourseStudents, CourseIterations=CourseIterations,
+                           CourseTrainer=CourseTrainer, CourseTrainerRate=CourseTrainerRate, CourseStudentOperationalCost=CourseStudentOperationalCost,
+                           courseMarketingCost=courseMarketingCost, courseTotalOperationalCost=courseTotalOperationalCost, courseTotalLicenseCost=courseTotalLicenseCost,
+                           courseTotalRevenue=courseTotalRevenue, courseProfit=courseProfit)
---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, IntegerField)
+from wtforms.validators import InputRequired, Length
+
+
+class CourseSimulator(FlaskForm):
+    denumireCurs = StringField('Denumire Curs', validators=[InputRequired(),
+                                                            Length(min=3, max=100)])
+    numarOre = IntegerField('Numar Ore', validators=[InputRequired()])
+    costCurs = IntegerField('Cost curs', validators=[InputRequired()])
+    costCompetitie = IntegerField('Avg. cost/h competitie', validators=[InputRequired()])
+    costLiceenta = IntegerField('Cost liceenta', validators=[InputRequired()])
+    students = IntegerField('Studenti/Iteratie', validators=[InputRequired()])
+    iteratii = IntegerField('Numar iteratii', validators=[InputRequired()])
+    numeProfesor = StringField('Nume profesor', validators=[InputRequired(),
+                                                            Length(min=3, max=100)])
+    tarifOrar = IntegerField('Tarif orar profesor', validators=[InputRequired()])
+    costOperational = IntegerField('Cost operational/student', validators=[InputRequired()])
+    costMarketing = IntegerField('Cost marketing', validators=[InputRequired()])
